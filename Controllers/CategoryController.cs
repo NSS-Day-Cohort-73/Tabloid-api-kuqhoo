09d524a5eaaac0d574702babc836df7640b0fab0
Tyler Parker <tyler.parker13@hotmail.com>
2025-02-12 10:17:27 -0600

    Category Controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Tabloid.Controllers;
 using Tabloid.Data;
+using Tabloid.Models;
 using Tabloid.Models.DTOs;
 
 [ApiController]
+// [Authorize]
 [Route("api/[controller]")]
 public class CategoryController : ControllerBase
 {
@@ -27,5 +30,31 @@ public class CategoryController : ControllerBase
                     // PostCount = c.PostCount, // can add later if we need the post count after it is updated in the database (or turned into a calculated property)
                 })
         );
+    }
+
+    [HttpPost]
+    public IActionResult CategoryPost(Category category)
+    {
+        _dbContext.Categories.Add(category);
+        _dbContext.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteCategory(int id)
+    {
+        Category categoryToDelete = _dbContext.Categories.Where(c => c.Id == id).FirstOrDefault();
+        _dbContext.Categories.Remove(categoryToDelete);
+        _dbContext.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateCategory(Category newCategory, int id)
+    {
+        Category categoryToUpdate = _dbContext.Categories.Where(c => c.Id == id).FirstOrDefault();
+        categoryToUpdate.Name = newCategory.Name;
+        _dbContext.SaveChanges();
+        return NoContent();
     }
 }
