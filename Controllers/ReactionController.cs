b4449cef672d447bec7b06b098f5c0739afe7d39
Tyler Parker <tyler.parker13@hotmail.com>
2025-02-13 16:04:15 -0600

    Commiting changes to test PR

---
+++ Controllers/ReactionController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tabloid.Data;
+using Tabloid.Models.DTOs;
+
+namespace Tabloid.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+// [Authorize]
+public class ReactionController : ControllerBase
+{
+    private TabloidDbContext _dbContext;
+
+    public ReactionController(TabloidDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet("types")]
+    public IActionResult GetReactionTypes()
+    {
+        List<ReactionTypeDTO> reactionTypes = _dbContext
+            .ReactionTypes.Select(rt => new ReactionTypeDTO
+            {
+                Id = rt.Id,
+                Type = rt.Type,
+                FaIcon = rt.FaIcon,
+            })
+            .ToList();
+
+        return Ok(reactionTypes);
+    }
+
+    [HttpGet("postreactions")]
+    public IActionResult GetPostReactions()
+    {
+        List<PostReactionDTO> postReactions = _dbContext
+            .PostReactions.Include(pr => pr.UserProfile)
+            .Include(pr => pr.Post)
+            .ThenInclude(p => p.Category)
+            .Include(pr => pr.Post)
+            .ThenInclude(p => p.UserProfile)
+            .Select(pr => new PostReactionDTO
+            {
+                Id = pr.Id,
+                UserProfileId = pr.UserProfileId,
+                PostId = pr.PostId,
+                ReactionTypeId = pr.ReactionTypeId,
+                CreatedAt = pr.CreatedAt,
+                ReactionType = _dbContext
+                    .ReactionTypes.Where(rt => rt.Id == pr.ReactionTypeId)
+                    .Select(rt => new ReactionTypeDTO
+                    {
+                        Id = rt.Id,
+                        Type = rt.Type,
+                        FaIcon = rt.FaIcon,
+                    })
+                    .FirstOrDefault(),
+                UserProfile = new UserProfileDTO
+                {
+                    Id = pr.UserProfile.Id,
+                    FirstName = pr.UserProfile.FirstName,
+                    LastName = pr.UserProfile.LastName,
+                    ImageLocation = pr.UserProfile.ImageLocation,
+                },
+                Post = new PostDTO
+                {
+                    Id = pr.Post.Id,
+                    Title = pr.Post.Title,
+                    Content = pr.Post.Content,
+                    HeaderImage = pr.Post.HeaderImage,
+                    CreatedAt = pr.Post.CreatedAt,
+                    IsApproved = pr.Post.IsApproved,
+                    CategoryName = pr.Post.Category.Name,
+                    Author = new UserProfileDTO
+                    {
+                        Id = pr.Post.UserProfile.Id,
+                        FirstName = pr.Post.UserProfile.FirstName,
+                        LastName = pr.Post.UserProfile.LastName,
+                        ImageLocation = pr.Post.UserProfile.ImageLocation,
+                    },
+                },
+            })
+            .ToList();
+
+        return Ok(postReactions);
+    }
+}
