0320450064e459f60538fd8c0184de085f6dd5e0
Rosemary Chadwick <rrchadwick@hotmail.com>
2025-02-21 10:20:41 -0600

    Tag Api working

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace Tabloid.Models;
+
 public class UserProfile
 {
     public int Id { get; set; }
@@ -36,9 +37,6 @@ public class UserProfile
 
     public string FullName
     {
-        get
-        {
-            return $"{FirstName} {LastName}";
-        }
+        get { return $"{FirstName} {LastName}"; }
     }
 }
